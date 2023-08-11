d9975ff34c7b4c74f797cd150968d1d3262326c5
miqueas <bagente24@gmail.com>
2023-08-11 14:35:04 -0300

    casi

--- src/controllers/img.controllers.js
+++ src/controllers/img.controllers.js
@@ -1,27 +1,26 @@
 const ctrlImg = {};
-const multer = require('multer');
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.filename + '-' + Date.now())
-    }
-})
+  destination: function (req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.filename + "-" + Date.now());
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
-ctrlImg.upload = upload.single('myFile')
+ctrlImg.upload = upload.single("myFile");
 
 ctrlImg.renderImg = (req, res) => {
-    res.render('index.ejs')
-}
-
-//Subir imagen 
-ctrlImg.Subirimagen = (req, res) => {
-    res.send({ data: 'Enviar un archivo' })
+  res.render("index.ejs");
 };
 
+//Subir imagen
+ctrlImg.Subirimagen = (req, res) => {
+  res.send({ data: "Enviar un archivo" });
+};
 
 module.exports = ctrlImg;
